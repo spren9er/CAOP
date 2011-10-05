2b8858a8a013f87c93f54e0e6881699c2c5d9902
Torsten Sprenger <ts@metaminded.com>
2011-10-05 16:42:07 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CAOP::Application.config.secret_token = 'cd7eb478e48c7f8fb0381f74da401be48d52aa2f32e974dc63c3cdd00b7eb0acbb6770ba57feea008e5d24bc2b1d3a7abce9ce8965b6edb0a2ca28857b6943e0'
